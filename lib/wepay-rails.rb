bb06cf0b6612347fece9459f8d5dac1bd1fe57a9
Adam Medeiros <adammede@gmail.com>
2011-07-17 22:17:26 -0700

    Building in the ability to collect User data through the wepay api

--- lib/wepay-rails.rb
+++ lib/wepay-rails.rb
@@ -4,14 +4,13 @@ module WepayRails
     require 'helpers/controller_helpers'
     class Gateway
       include HTTParty
+      base_uri Rails.env.production? ? "https://api.wepay.com" : "https://stage.wepay.com"
 
       attr_accessor :wepay_auth_code
 
       def initialize(*args)
         yml = Rails.root.join('config', 'wepay.yml').to_s
         @config = YAML.load_file(yml)[Rails.env].symbolize_keys
-        @base_uri = Rails.env.production? ? "https://api.wepay.com" : "https://stage.wepay.com"
-        base_uri @base_uri
       end
 
       def wepay_auth_header
