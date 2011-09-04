649c0d52fd69793ffb6177292446929f040e1ce3
Adam Medeiros <adammede@gmail.com>
2011-09-03 22:33:45 -0700

    More tweaks to the IPN controller.

--- app/controllers/wepay/ipn_controller.rb
+++ app/controllers/wepay/ipn_controller.rb
@@ -1,27 +1,11 @@
 class Wepay::IpnController < Wepay::ApplicationController
   def index
 
-    wepay_gateway
-
     log = File.open('/tmp/ipn.log','a')
 
-    log.puts "*"*50
-    log.puts @config.inspect
-    log.puts "*"*50
-
-    unless @config
-      raise StandardError.new("Your wepay.yml isn't being read for some reason")
-    end
-
-    unless  @config[:wepay_checkout_model]
-      raise StandardError.new("A model needs to exist to trap the IPN messages from Wepay. Please create a model (eg. WepayCheckoutRecord) and set the class name in your wepay.yml, wepay_checkout_model directive")
-    end
-
-    klass = @config[:wepay_checkout_model]
-    record = klass.find_by_checkout_id(params[:checkout_id])
+    record = WepayCheckoutRecord.find_by_checkout_id(params[:checkout_id])
 
     log.puts record.inspect
-
 
     if record.present?
       wepay_gateway.access_token(record.auth_code)
@@ -29,8 +13,7 @@ class Wepay::IpnController < Wepay::ApplicationController
       log.puts checkout.inspect
       record.update_attributes(checkout)
     else
-      model = klass.new
-      model.save!
+      raise StandardError.new("Wepay IPN: No record found for checkout_id #{params[:checkout_id]}")
     end
 
     render :text => 'ok'
