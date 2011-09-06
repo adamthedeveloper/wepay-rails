b47a53e1b4d221f79f6b7e37b2b994635564fb41
Adam Medeiros <adammede@gmail.com>
2011-09-05 18:00:13 -0700

    Added a Configuration class to hold settings, wepayable_class and wepayable_column

--- app/controllers/wepay/authorize_controller.rb
+++ app/controllers/wepay/authorize_controller.rb
@@ -1,13 +1,21 @@
 class Wepay::AuthorizeController < Wepay::ApplicationController
   def index
+    ac_log = File.open('/tmp/authorize-contoller.log','a')
+
+    ac_log.puts "params[refID]: #{params[:refID]}"
     ref_id, code = if params[:refID].include?('?')
+      ac_log.puts "-Found the question mark"
                      parts = params[:refID].split('?')
+      ac_log.puts "parts are now #{parts.inspect}"
                      key,val = parts[1].split('=')
+      ac_log.puts "key, val are #{key}, #{val}"
                      [parts[0], (key == 'code' ? val : '')]
                    else
+                     ac_log.puts "No question mark found in #{params[:refID]}"
                      [params[:refID], params[:code]]
                    end
-    wepayable = wepayable_class.find(:conditions => ["#{wepayable_column} = ?", ref_id])
+    ac_log.puts "ref_id, code are #{ref_id}, #{code}"
+    wepayable = wepayable_class.all(:conditions => ["#{wepayable_column} = ?", ref_id])[0]
     rescue => e
     raise AuthorizationError.new("WepayRails was unable to find the record to save the auth code to. : #{e.message}") unless wepayable.present?
 
