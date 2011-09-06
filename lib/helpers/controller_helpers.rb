810f49177682c5bcdf962a6709e8e3a2fe711b93
Adam Medeiros <adammede@gmail.com>
2011-09-05 20:07:06 -0700

    Added a Configuration class to hold settings, wepayable_class and wepayable_column

--- lib/helpers/controller_helpers.rb
+++ lib/helpers/controller_helpers.rb
@@ -99,7 +99,7 @@ module WepayRails
       # :charge_tax	No	A boolean value (0 or 1). If set to 1 and the account has a relevant tax entry (see /account/set_tax), then tax will be charged.
       def init_checkout_and_send_user_to_wepay(parms)
         response = wepay_gateway.perform_checkout(parms)
-        raise WepayRails::Exceptions::InitializeCheckoutError.new("A problem occurred while trying to checkout. Wepay didn't send us back a checkout uri") unless response && response.has_key?('checkout_uri')
+        raise WepayRails::Exceptions::InitializeCheckoutError.new("A problem occurred while trying to checkout. Wepay didn't send us back a checkout uri. Response was: #{response.inspect}") unless response && response.has_key?('checkout_uri')
         redirect_to response['checkout_uri'] and return
       end
     end
