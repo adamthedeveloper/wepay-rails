ecea7696fb8652c6f1bcc61432f9eda4b14916fe
Adam Medeiros <adammede@gmail.com>
2011-09-05 17:33:46 -0700

    Added a Configuration class to hold settings, wepayable_class and wepayable_column

--- app/controllers/wepay/authorize_controller.rb
+++ app/controllers/wepay/authorize_controller.rb
@@ -1,11 +1,17 @@
 class Wepay::AuthorizeController < Wepay::ApplicationController
   def index
-    ref_id = params[:refID]
+    ref_id, code = if params[:refID].include?('?')
+                     parts = query.split('?')
+                     key,val = parts[1].split('=')
+                     [parts[0], (key == 'code' ? val : '')]
+                   else
+                     [params[:refID], params[:code]]
+                   end
     wepayable = wepayable_class.find(:conditions => ["#{wepayable_column} = ?", ref_id])
     rescue => e
     raise AuthorizationError.new("WepayRails was unable to find the record to save the auth code to. : #{e.message}") unless wepayable.present?
 
-    wepayable.update_attribute(wepayable_column.to_sym, params[:code])
+    wepayable.update_attribute(wepayable_column.to_sym, code)
 
     redirect_to WepayRails::Configuration.settings[:after_authorize_redirect_uri]
   end
