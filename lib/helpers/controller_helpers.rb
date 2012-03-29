ddd1c877f31733ee1498ecc605f25566738cd7c7
Gilbert <gilbertbgarza@gmail.com>
2012-03-29 16:11:24 -0500

    Divided controller helper into checkout	and redirect methods.
    This allows one to create a checkout without redirecting and instead
    use the checkout_uri for a wepay iframe.
    
    https://www.wepay.com/developer/tutorial/iframe

--- lib/helpers/controller_helpers.rb
+++ lib/helpers/controller_helpers.rb
@@ -37,7 +37,7 @@ module WepayRails
       # :require_shipping	No	A boolean value (0 or 1). If set to 1 then the payer will be asked to enter a shipping address when they pay. After payment you can retrieve this shipping address by calling /checkout
       # :shipping_fee	No	The amount that you want to charge for shipping.
       # :charge_tax	No	A boolean value (0 or 1). If set to 1 and the account has a relevant tax entry (see /account/set_tax), then tax will be charged.
-      def init_checkout_and_send_user_to_wepay(params, access_token=nil)
+      def init_checkout(params, access_token=nil)
         wepay_gateway = WepayRails::Payments::Gateway.new(access_token)
         response      = wepay_gateway.perform_checkout(params)
 
@@ -48,13 +48,19 @@ module WepayRails
         params.merge!({
             :access_token   => wepay_gateway.access_token,
             :checkout_id    => response[:checkout_id],
-            :security_token => response[:security_token]
+            :security_token => response[:security_token],
+            :checkout_uri   => response[:checkout_uri]
         })
 
         WepayCheckoutRecord.create(params)
-
-        redirect_to response[:checkout_uri] and return
       end
+
+      def init_checkout_and_send_user_to_wepay(params, access_token=nil)
+        record = init_checkout(params, access_token)
+        redirect_to record.checkout_uri and return
+      end
+
+
     end
   end
 end
