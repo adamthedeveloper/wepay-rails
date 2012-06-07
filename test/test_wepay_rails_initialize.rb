8795e608f3068a465587615bbc20bd1dfa72b964
Steve Aquino <aquino.steve@gmail.com>
2012-06-07 06:05:46 -0700

    Change name of test class and change local variables into object variables for clarity.

--- test/test_wepay_rails_initialize.rb
+++ test/test_wepay_rails_initialize.rb
@@ -1,6 +1,6 @@
 require File.expand_path(File.dirname(__FILE__) + '/helper')
 
-class TestWepayRails < ActiveSupport::TestCase
+class TestWepayRailsInitialize < ActiveSupport::TestCase
   def teardown
     delete_wepay_config_file
   end
@@ -8,24 +8,24 @@ class TestWepayRails < ActiveSupport::TestCase
   test "should initialize WepayRails with settings from wepay.yml" do
     create_wepay_config_file
     initialize_wepay_config
-    gateway = WepayRails::Payments::Gateway.new
+    @gateway = WepayRails::Payments::Gateway.new
 
-    assert_not_nil gateway.configuration
-    assert_equal "http://www.example.com", gateway.configuration[:root_callback_uri]
+    assert_not_nil @gateway.configuration
+    assert_equal "http://www.example.com", @gateway.configuration[:root_callback_uri]
   end
 
   test "should initialize WepayRails with embedded Ruby in wepay.yml.erb" do
     create_wepay_config_file(true)
     initialize_wepay_config
-    gateway = WepayRails::Payments::Gateway.new
+    @gateway = WepayRails::Payments::Gateway.new
 
-    assert_not_nil gateway.configuration
-    assert_equal "http://www.example.com", gateway.configuration[:root_callback_uri]
-    assert_equal 'abc' * 3, gateway.access_token
+    assert_not_nil @gateway.configuration
+    assert_equal "http://www.example.com", @gateway.configuration[:root_callback_uri]
+    assert_equal 'abc' * 3, @gateway.access_token
   end
 
   test "should initialize WepayRails with an existing access_token" do
-    gateway = WepayRails::Payments::Gateway.new("myAccessToken")
-    assert_equal "myAccessToken", gateway.access_token
+    @gateway = WepayRails::Payments::Gateway.new("myAccessToken")
+    assert_equal "myAccessToken", @gateway.access_token
   end
 end
