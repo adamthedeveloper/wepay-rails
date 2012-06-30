818c997307ae19c5aa8d5543a9306a518ffc4523
Alex Perelman <alex@alexperelman.com>
2012-06-30 11:06:08 -0700

    Removed unnecessary attributes before updating checkout records

--- app/controllers/wepay/checkout_controller.rb
+++ app/controllers/wepay/checkout_controller.rb
@@ -3,8 +3,12 @@ class Wepay::CheckoutController < Wepay::ApplicationController
     record = WepayCheckoutRecord.find_by_checkout_id_and_security_token(params[:checkout_id],params[:security_token])
 
     if record.present?
-      wepay_gateway = WepayRails::Payments::Gateway.new
+      wepay_gateway = WepayRails::Payments::Gateway.new( record.access_token )
       checkout = wepay_gateway.lookup_checkout(record.checkout_id)
+      
+      # remove unnecessary attributes
+      checkout.delete_if {|k,v| !record.attributes.include? k.to_s}
+      
       record.update_attributes(checkout)
       redirect_to "#{wepay_gateway.configuration[:after_checkout_redirect_uri]}?checkout_id=#{params[:checkout_id]}"
     else
