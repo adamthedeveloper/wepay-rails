91deac9a0fa2abcfed1690a4cdcb28a670ae31ff
Steve Aquino <aquino.steve@gmail.com>
2012-09-16 10:47:28 -0700

    Remove extra whitespace

--- lib/wepay-rails.rb
+++ lib/wepay-rails.rb
@@ -113,37 +113,37 @@ module WepayRails
       end
 
       def call_api(api_path, params={}, timeout=30000)
-	    begin
-		  self.class.default_timeout(timeout)
-		  response = self.class.post("#{@api_endpoint}#{api_path}", {:headers => wepay_auth_header, :body => params})
-		  json = symbolize_response(response.body)
-	    rescue Errno, JSON::ParserError => e
-	      raise WepayRails::Exceptions::WepayApiError.new("The request to WePay timed out.  This might mean you sent an invalid request or WePay is having issues.")
-	    rescue => e
-		  raise e if e.class.to_s =~ /WepayRails/
-		  raise WepayRails::Exceptions::WepayApiError.new("There was an error while trying to connect with WePay - #{e.class}: #{e.message}")
-	    end
-	    if response.success?
-		  return json
-	    elsif response.code == 401
-		  raise WepayRails::Exceptions::ExpiredTokenError.new("Token either expired, revoked or invalid: #{json.inspect}.")
-	    else
-		  raise WepayRails::Exceptions::WepayApiError.new("The API request failed with error code ##{response.code}: #{json.inspect}.")
-	    end
+	begin
+	  self.class.default_timeout(timeout)
+	  response = self.class.post("#{@api_endpoint}#{api_path}", {:headers => wepay_auth_header, :body => params})
+	  json = symbolize_response(response.body)
+	rescue Errno, JSON::ParserError => e
+	  raise WepayRails::Exceptions::WepayApiError.new("The request to WePay timed out.  This might mean you sent an invalid request or WePay is having issues.")
+	rescue => e
+	  raise e if e.class.to_s =~ /WepayRails/
+	  raise WepayRails::Exceptions::WepayApiError.new("There was an error while trying to connect with WePay - #{e.class}: #{e.message}")
+	end
+	if response.success?
+	  return json
+	elsif response.code == 401
+	  raise WepayRails::Exceptions::ExpiredTokenError.new("Token either expired, revoked or invalid: #{json.inspect}.")
+	else
+	  raise WepayRails::Exceptions::WepayApiError.new("The API request failed with error code ##{response.code}: #{json.inspect}.")
+	end
       end
 
       # Fetch the access token from wepay for the auth code
       def get_access_token(auth_code, redirect_uri)
-	    params = {
-		  :client_id     => @wepay_config[:client_id],
-		  :client_secret => @wepay_config[:client_secret],
-		  :redirect_uri  => redirect_uri,
-		  :code          => auth_code
-	    }
-	    json = call_api("/oauth2/token", params)
-	    raise WepayRails::Exceptions::AccessTokenError.new("A problem occurred trying to get the access token: #{json.inspect}") unless json.has_key?(:access_token)
-	    @account_id   = json[:user_id]
-	    @access_token = json[:access_token]
+	params = {
+	  :client_id     => @wepay_config[:client_id],
+	  :client_secret => @wepay_config[:client_secret],
+	  :redirect_uri  => redirect_uri,
+	  :code          => auth_code
+	}
+	json = call_api("/oauth2/token", params)
+	raise WepayRails::Exceptions::AccessTokenError.new("A problem occurred trying to get the access token: #{json.inspect}") unless json.has_key?(:access_token)
+	@account_id   = json[:user_id]
+	@access_token = json[:access_token]
       end
 
       include WepayRails::Api::AccountMethods
